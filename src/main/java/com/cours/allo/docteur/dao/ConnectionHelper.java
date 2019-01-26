ad225f50ee193bf57d9811411e8bff796afdfc17
Mohamed Amine AMGHAR <mohamed.amghar@cgi.com>
2019-01-26 13:34:42 +0100

    first commit

---
+++ src/main/java/com/cours/allo/docteur/dao/ConnectionHelper.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cours.allo.docteur.dao;
+
+import java.sql.Connection;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ *
+ * @author ElHadji
+ */
+public class ConnectionHelper {
+
+    private static final Log log = LogFactory.getLog(ConnectionHelper.class);
+    public final static String className = ConnectionHelper.class.getName();
+
+    public static void closeSqlResources(Connection connection) {
+    }
+}
---
+++ src/main/java/com/cours/allo/docteur/service/ServiceFacade.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cours.allo.docteur.service;
+
+import com.cours.allo.docteur.dao.IAdresseDao;
+import com.cours.allo.docteur.dao.IUtilisateurDao;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ *
+ * @author ElHadji
+ */
+public class ServiceFacade implements IServiceFacade {
+
+    private static final Log log = LogFactory.getLog(ServiceFacade.class);
+    // On liste toutes les DAO : un DAO pour chaque entité (Utilisateur,Adresse ect ....)
+
+    private IUtilisateurDao utilisateurDao = null;
+
+    private IAdresseDao adresseDao = null;
+
+    @Override
+    public IUtilisateurDao getUtilisateurDao() {
+        return utilisateurDao;
+    }
+
+    @Override
+    public IAdresseDao getAdresseDao() {
+        return adresseDao;
+    }
+}
